0cda76ac90a85910f375335778632217ef6b2f2d
Sarju Thakkar <47423423+SarjuThakkar@users.noreply.github.com>
2026-01-18 01:44:43 -0600

    Update fundraising and mileage progress

--- constants.js
+++ constants.js
@@ -3,9 +3,9 @@
 
 window.MARATHON_DATA = {
     fundraisingGoal: 2500,
-    fundraisingRaised: 715,
+    fundraisingRaised: 790,
     mileageGoal: 1000,
-    mileageRan: 14,
+    mileageRan: 20,
     raceDate: "2026-10-11T08:00:00",
     fundraisingUrl: "https://www.classy.org/fundraiser/6888759"
 };
